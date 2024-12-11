eced4ac2ec3e4610722ecd28e59a535949aceaf9
ImDuong <shiroyasha699969@gmail.com>
2024-12-12 00:49:50 +0700

    add: utils for upper bound and lower bound extension

--- internal/core/utils/search.go
+++ internal/core/utils/search.go
@@ -24,6 +24,7 @@ func BinarySearch(nums []int, startIdx, endIdx, target int) int {
 // return the index of the first element in range [first, last)
 // - where all elements in this range does not less than target
 // - if range is empty, return last
+// in short, find index of the smallest x such that x >= target
 func LowerBound(nums []int, target int) int {
 	low, high, mid := 0, len(nums)-1, 0
 	for low <= high {
@@ -35,4 +36,49 @@ func LowerBound(nums []int, target int) int {
 		}
 	}
 	return low
+}
+
+// equivalents to std::upper_bound in c++
+// return the index of the first element in range [first, last)
+// - where all elements in this range is strictly greater than the target
+// find index of the smallest x such that x > target
+func UpperBound(nums []int, target int) int {
+	low, high, mid := 0, len(nums)-1, 0
+	for low <= high {
+		mid = low + (high-low)/2
+		if nums[mid] <= target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return low
+}
+
+// find index of the biggest x such that x < target
+func LowerBoundNegation(nums []int, target int) int {
+	low, high, mid := 0, len(nums)-1, 0
+	for low <= high {
+		mid = low + (high-low)/2
+		if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return high
+}
+
+// find index of the biggest x such that x <= target
+func UpperBoundNegation(nums []int, target int) int {
+	low, high, mid := 0, len(nums)-1, 0
+	for low <= high {
+		mid = low + (high-low)/2
+		if nums[mid] <= target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return high
 }
