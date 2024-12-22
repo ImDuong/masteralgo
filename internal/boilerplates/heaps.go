e3a68e701009aaa29c2b36d193d975ae3e30aa0f
ImDuong <shiroyasha699969@gmail.com>
2024-12-23 00:14:02 +0700

    refactor: move max int heap to boilerplate code

--- internal/boilerplates/heaps.go
+++ internal/boilerplates/heaps.go
@@ -1,5 +1,7 @@
 package boilerplates
 
+import "container/heap"
+
 type StringMinHeap []string
 
 func (h StringMinHeap) Len() int           { return len(h) }
@@ -18,4 +20,47 @@ func (h *StringMinHeap) Pop() interface{} {
 
 func (h *StringMinHeap) Top() string {
 	return (*h)[0]
+}
+
+// h := &IntMinHeap{}
+// heap.Init(h)
+type IntMinHeap []int
+
+func (h IntMinHeap) Len() int           { return len(h) }
+func (h IntMinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntMinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntMinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntMinHeap) Pop() interface{} {
+	poppedItem := (*h)[h.Len()-1]
+	*h = (*h)[0 : h.Len()-1]
+	return poppedItem
+}
+
+func (h *IntMinHeap) Top() int {
+	return (*h)[0]
+}
+
+type IntMaxHeap []int
+
+func (h IntMaxHeap) Len() int           { return len(h) }
+func (h IntMaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntMaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntMaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntMaxHeap) Pop() interface{} {
+	poppedItem := (*h)[h.Len()-1]
+	*h = (*h)[0 : h.Len()-1]
+	return poppedItem
+}
+
+func (h *IntMaxHeap) Update(idx int, value int) {
+	(*h)[idx] = value
+	heap.Fix(h, idx)
 }
--- leetcode/heap_priority_queue/2530-maximal_score_after_applying_k_operations.go
+++ leetcode/heap_priority_queue/2530-maximal_score_after_applying_k_operations.go
@@ -2,6 +2,7 @@ package heap_priority_queue
 
 import (
 	"container/heap"
+	"masteralgo/internal/boilerplates"
 )
 
 func Challenge2530(nums []int, k int) int64 {
@@ -9,7 +10,7 @@ func Challenge2530(nums []int, k int) int64 {
 }
 
 func maxKelements(nums []int, k int) int64 {
-	h2 := IntMaxHeap(nums)
+	h2 := boilerplates.IntMaxHeap(nums)
 	h := &h2
 	heap.Init(h)
 	var rs int64 = 0
@@ -17,29 +18,8 @@ func maxKelements(nums []int, k int) int64 {
 		curMax := (*h)[0]
 		rs += int64(curMax)
 
-		h.update(0, (curMax+3-1)/3)
+		h.Update(0, (curMax+3-1)/3)
 		k--
 	}
 	return rs
-}
-
-type IntMaxHeap []int
-
-func (h IntMaxHeap) Len() int           { return len(h) }
-func (h IntMaxHeap) Less(i, j int) bool { return h[i] > h[j] }
-func (h IntMaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *IntMaxHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
-}
-
-func (h *IntMaxHeap) Pop() interface{} {
-	poppedItem := (*h)[h.Len()-1]
-	*h = (*h)[0 : h.Len()-1]
-	return poppedItem
-}
-
-func (h *IntMaxHeap) update(idx int, value int) {
-	(*h)[idx] = value
-	heap.Fix(h, idx)
 }
