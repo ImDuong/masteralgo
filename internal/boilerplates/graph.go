68f5cad06a6be96ce535c84e5628397b32ce556f
ImDuong <shiroyasha699969@gmail.com>
2025-04-26 01:38:33 +0700

    add: graph tips

---
+++ internal/boilerplates/graph.go
@@ -0,0 +1,5 @@
+package boilerplates
+
+// Use Dijkstra’s algorithm for shortest paths with positive weights.
+// Use Floyd-Warshall for all-pairs shortest paths (good for smaller graphs).
+// Use Bellman-Ford if negative weights or negative cycles are involved.
